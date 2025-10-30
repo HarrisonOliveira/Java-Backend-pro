4370a1bcea30fd6543d4c227f81eda428153d07f
Harrison <harrison@dev.com>
2025-10-30 10:26:20 -0500

    Initial commit

---
+++ mod9/projeto-demo/src/test/java/com/ebac/projeto_demo/ProjetoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebac.projeto_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
